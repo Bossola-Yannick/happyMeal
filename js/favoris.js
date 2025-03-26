2cff89ddeccb9a967e2b7cbc7cff83509989df48
James Sanchez <james.sanchez@laplateforme.io>
2025-03-26 13:32:37 +0100

    fix: display favorites recipes cards

--- js/favoris.js
+++ js/favoris.js
@@ -3,16 +3,15 @@
 // let allRecipe = JSON.parse(localStorage.getItem("all-recipe"));
 // console.log(allRecipe);
 let favoritesRecipes;
-if (!localStorage.getItem("favorites")) {
+if (localStorage.getItem("favorite") !== null) {
   favoritesRecipes = JSON.parse(localStorage.getItem("favorite"));
 } else {
   favoritesRecipes = allRecipe.slice(3, 8);
   localStorage.setItem("favorite", JSON.stringify(favoritesRecipes));
+  favoritesRecipes = JSON.parse(localStorage.getItem("favorite"));
 }
-// let favoritesRecipes = allRecipe;
-// let favoritesRecipes = [];
 
-// console.log(favoritesRecipes);
+console.log(favoritesRecipes);
 
 // VARIABLE
 let listeCat = [];
@@ -51,131 +50,153 @@ const closeModal = () => {
 };
 
 // creer la modale avec les infos de la recette selectionnée
-const createModal = () => {
-  const indexRecipeClicked = localStorage.getItem("id-clicked-recipe");
-  const recipeClicked = favoritesRecipes[indexRecipeClicked];
-  // console.log(recipeClicked);
+// const createModal = () => {
+//   const indexRecipeClicked = localStorage.getItem("id-clicked-recipe");
+//   const recipeClicked = favoritesRecipes[indexRecipeClicked];
+//   // console.log(recipeClicked);
 
-  // boite modale
-  const modalBox = document.createElement("div");
-  modalBox.id = "modal";
-  modalBox.classList.add("modal", "scroller");
+//   // boite modale
+//   const modalBox = document.createElement("div");
+//   modalBox.id = "modal";
+//   modalBox.classList.add("modal", "scroller");
 
-  // boite modale close
-  const modalClose = document.createElement("div");
-  modalClose.id = "modal-close";
-  modalClose.classList.add("modal-close");
+//   // boite modale close
+// const modalClose = document.createElement("div");
+// modalClose.id = "modal-close";
+// modalClose.classList.add("modal-close");
 
-  const modalCloseIcon = document.createElement("img");
-  modalCloseIcon.setAttribute("src", "../assets/img/icon-close.png");
-  modalClose.appendChild(modalCloseIcon);
-  modalCloseIcon.addEventListener("click", closeModal);
+// const modalCloseIcon = document.createElement("img");
+// modalCloseIcon.setAttribute("src", "../assets/img/icon-close.png");
+// modalClose.appendChild(modalCloseIcon);
+// modalCloseIcon.addEventListener("click", closeModal);
 
-  // boite modale header
-  const modalHeader = document.createElement("div");
-  modalHeader.classList.add("modal-header");
+//   // boite modale header
+//   const modalHeader = document.createElement("div");
+//   modalHeader.classList.add("modal-header");
 
-  const modalH2 = document.createElement("h2");
-  modalH2.innerText = "{recette}";
+//   const modalH2 = document.createElement("h2");
+//   modalH2.innerText = "{recette}";
 
-  const modalP1 = document.createElement("p");
-  modalP1.innerText = "{temps preparation}";
+//   const modalP1 = document.createElement("p");
+//   modalP1.innerText = "{temps preparation}";
 
-  modalHeader.appendChild(modalH2);
-  modalHeader.appendChild(modalP1);
+//   modalHeader.appendChild(modalH2);
+//   modalHeader.appendChild(modalP1);
 
-  // boite modale sub header
-  const modalSubHeader = document.createElement("div");
-  modalSubHeader.classList.add("modal-sub-header");
+//   // boite modale sub header
+//   const modalSubHeader = document.createElement("div");
+//   modalSubHeader.classList.add("modal-sub-header");
 
-  const modalSubHeaderH5 = document.createElement("h5");
-  modalSubHeaderH5.innerText = "Ajouter aux menus de la semaine ?";
+//   const modalSubHeaderH5 = document.createElement("h5");
+//   modalSubHeaderH5.innerText = "Ajouter aux menus de la semaine ?";
 
-  const modalSubHeaderSelect = document.createElement("select");
-  const modalSelectEx = document.createElement("option");
-  modalSelectEx.innerText = "--Choisir un jour de la semaine--";
-  modalSubHeaderSelect.appendChild(modalSelectEx);
-  const jourSemaine = [
-    "lundi",
-    "mardi",
-    "mercredi",
-    "jeudi",
-    "vendredi",
-    "samedi",
-    "dimanche",
-  ];
-  jourSemaine.forEach((jour) => {
-    const modalSelectOption = document.createElement("option");
-    modalSelectOption.innerText = jour;
-    modalSubHeaderSelect.appendChild(modalSelectOption);
-  });
+//   const modalSubHeaderSelect = document.createElement("select");
+//   const modalSelectEx = document.createElement("option");
+//   modalSelectEx.innerText = "--Choisir un jour de la semaine--";
+//   modalSubHeaderSelect.appendChild(modalSelectEx);
+//   const jourSemaine = [
+//     "lundi",
+//     "mardi",
+//     "mercredi",
+//     "jeudi",
+//     "vendredi",
+//     "samedi",
+//     "dimanche",
+//   ];
+//   jourSemaine.forEach((jour) => {
+//     const modalSelectOption = document.createElement("option");
+//     modalSelectOption.innerText = jour;
+//     modalSubHeaderSelect.appendChild(modalSelectOption);
+//   });
 
-  modalSubHeader.appendChild(modalSubHeaderH5);
-  modalSubHeader.appendChild(modalSubHeaderSelect);
+//   modalSubHeader.appendChild(modalSubHeaderH5);
+//   modalSubHeader.appendChild(modalSubHeaderSelect);
 
-  // boite modale body
-  const modalBody = document.createElement("div");
-  modalBody.classList.add("modal-body");
+//   // boite modale body
+//   const modalBody = document.createElement("div");
+//   modalBody.classList.add("modal-body");
 
-  const modalListIngredients = document.createElement("ul");
-  modalListIngredients.classList.add("ingredients");
-  modalBody.appendChild(modalListIngredients);
+//   const modalListIngredients = document.createElement("ul");
+//   modalListIngredients.classList.add("ingredients");
+//   modalBody.appendChild(modalListIngredients);
 
-  const modalIngredients = document.createElement("li");
-  modalIngredients.innerHTML = `(ingredient nom) <span>(quantite)</span>`;
-  modalListIngredients.appendChild(modalIngredients);
+//   const modalIngredients = document.createElement("li");
+//   modalIngredients.innerHTML = `(ingredient nom) <span>(quantite)</span>`;
+//   modalListIngredients.appendChild(modalIngredients);
 
-  const modalCookingStepsList = document.createElement("div");
-  modalCookingStepsList.classList.add("cooking-steps");
+//   const modalCookingStepsList = document.createElement("div");
+//   modalCookingStepsList.classList.add("cooking-steps");
 
-  const modalCookingH3 = document.createElement("h3");
-  modalCookingH3.innerText = "Etapes";
-  modalCookingStepsList.appendChild(modalCookingH3);
-  const modalCookingOl = document.createElement("ol");
-  const modalCookingLi = document.createElement("li");
-  modalCookingLi.innerText = "etapes X";
-  modalCookingOl.appendChild(modalCookingLi);
-  modalCookingStepsList.appendChild(modalCookingOl);
+//   const modalCookingH3 = document.createElement("h3");
+//   modalCookingH3.innerText = "Etapes";
+//   modalCookingStepsList.appendChild(modalCookingH3);
+//   const modalCookingOl = document.createElement("ol");
+//   const modalCookingLi = document.createElement("li");
+//   modalCookingLi.innerText = "etapes X";
+//   modalCookingOl.appendChild(modalCookingLi);
+//   modalCookingStepsList.appendChild(modalCookingOl);
 
-  // append toutes les boites dans modale box
-  modalBox.appendChild(modalClose);
-  modalBox.appendChild(modalHeader);
-  modalBox.appendChild(modalSubHeader);
-  modalBox.appendChild(modalBody);
-  modalBox.appendChild(modalCookingStepsList);
-  document.body.appendChild(modalBox);
-  console.log(modalBox);
-};
+//   // append toutes les boites dans modale box
+//   modalBox.appendChild(modalClose);
+//   modalBox.appendChild(modalHeader);
+//   modalBox.appendChild(modalSubHeader);
+//   modalBox.appendChild(modalBody);
+//   modalBox.appendChild(modalCookingStepsList);
+//   document.body.appendChild(modalBox);
+//   console.log(modalBox);
+// };
+
+// const createModal = (index) => {
+//   const modalBox = document.getElementById("modal");
+
+//   // const recipeChoose = localStorage.getItem("favorite");
+//   // console.log(recipeChoose);
+
+//   const modalClose = document.createElement("div");
+//   modalClose.id = "modal-close";
+//   modalClose.classList.add("modal-close");
+
+//   const modalCloseIcon = document.createElement("img");
+//   modalCloseIcon.setAttribute("src", "../assets/img/icon-close.png");
+//   modalClose.appendChild(modalCloseIcon);
+//   modalCloseIcon.addEventListener("click", closeModal);
+//   modalBox.appendChild(modalClose);
+
+//   favoritesRecipes.forEach((recette) => {
+//     if (favoritesRecipes.indexOf(recette) === index) {
+//       console.log(recette.nom);
+//       modalBox.innerHTML += `
+//         <div class="modal-header">
+//           <h2>${recette.nom}</h2>
+//           <p>${recette.temps_preparation}</p>
+//         </div>
+//         <div class="modal-body">
+//           <ul class="ingredients">
+//             ${recette.ingredients.map((ingredient) => `<li>${ingredient}</li>`)}
+//           </ul>
+//           <div class="cooking-steps">
+//             <h3>{étapes}</h3>
+//             <ol>
+//               <li>Lorem ipsum dolor sit</li>
+//             </ol>
+//           </div>
+//         </div>
+//       `;
+//     }
+//   });
+// };
+
+// createModal(1);
 
 // affiche la liste des recipes favorites
+
 const showListFav = () => {
-  let listFav = {
-    favorites: [],
-  };
-
-  if (favoritesRecipes) {
-    favoritesRecipes.forEach((recipe) => {
-      recipe.ingredients = recipe.ingredients.map((ingredient) => {
-        if (ingredient.nom && ingredient.nom.includes("(")) {
-          return (ingredient = ingredient.nom.split("(")[0]);
-        } else if (ingredient.nom) {
-          return (ingredient = ingredient.nom);
-        } else {
-          return ingredient;
-        }
-      });
-      listFav.favorites.push(recipe);
-    });
-  }
-
-  // console.log(listFav);
-
   // affichage liste
-  if (listFav.favorites.length > 0) {
+  if (favoritesRecipes.length > 0) {
     listFavBox.innerHTML = "";
 
-    listFav.favorites.forEach((recipe) => {
-      const indexRecipe = listFav.favorites.indexOf(recipe);
+    favoritesRecipes.forEach((recipe) => {
+      const indexRecipe = favoritesRecipes.indexOf(recipe);
 
       // boite recette
       const secondCard = document.createElement("div");
@@ -190,10 +211,10 @@ const showListFav = () => {
 
       buttonFav.addEventListener("click", () => {
         favoritesRecipes.splice(indexRecipe, 1);
+        console.log("coucou");
         showListFav();
         localStorage.setItem("favorite", JSON.stringify(favoritesRecipes));
         console.log(favoritesRecipes);
-        console.log(listFav);
       });
 
       cardButton.appendChild(buttonFav);
@@ -205,8 +226,8 @@ const showListFav = () => {
       card.classList.add("card-box");
       card.setAttribute("value", indexRecipe);
       card.addEventListener("click", () => {
-        localStorage.setItem("id-clicked-recipe", indexRecipe);
-        openModal();
+        // localStorage.setItem("id-clicked-recipe", indexRecipe);
+        openModal(indexRecipe);
       });
 
       // ajoute le titre et la catégorie
@@ -221,10 +242,10 @@ const showListFav = () => {
       // boite pour les ingrédients
       const cardDesc = document.createElement("div");
       cardDesc.id = "card-desc";
-      recipe.ingredients.forEach((ingredient) => {
+      recipe.ingredients.map((ingredient) => {
         const pIngredient = document.createElement("p");
         pIngredient.classList.add("tag-ingredient");
-        pIngredient.textContent = ingredient;
+        pIngredient.textContent = ingredient.nom;
         cardDesc.appendChild(pIngredient);
       });
       card.appendChild(cardDesc);
@@ -232,7 +253,7 @@ const showListFav = () => {
       // Ajoute le footer
       const cardFooter = document.createElement("div");
       cardFooter.id = "card-footer";
-      cardFooter.innerHTML = `<p>${recipe.temps_preparation}</p>`;
+      cardFooter.innerHTML = `<p>Préparation: ${recipe.temps_preparation}</p>`;
       card.appendChild(cardFooter);
 
       // Ajoute la carte au conteneur principal
@@ -247,4 +268,4 @@ const showListFav = () => {
 };
 
 showListFav();
-createModal();
+// createModal();

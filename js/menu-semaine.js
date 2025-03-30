368272a3ab65d7d45879e38238689807c8f249b9
James Sanchez <james.sanchez@laplateforme.io>
2025-03-30 21:06:57 +0200

    add: working drag and drop

--- js/menu-semaine.js
+++ js/menu-semaine.js
@@ -8,39 +8,131 @@ if (localStorage.getItem("recipes-week") !== null) {
   recipesWeek = JSON.parse(localStorage.getItem("recipes-week"));
 }
 
+// affiche planning
+const showPlanning = () => {
+  const weekDay = [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche",
+  ];
+
+  const weekDayContainer = document.getElementById("week-day-container");
+
+  weekDay.forEach((day) => {
+    weekDayContainer.innerHTML += `
+    <div class="day">
+          <h2>${day}</h2>
+          <div class="box-planning">
+            <!-- midi -->
+            <div id="time-day" class="time-day">
+              <h4 class="title-meal">Midi</h4>
+              <div class="meal-time-box">
+                <div class="meal-box">
+                  <h3>Entrée</h3>
+                  <div class="drop-recipe"></div>
+                </div>
+                <div class="meal-box">
+                  <h3>Plat</h3>
+                  <div class="drop-recipe"></div>
+                </div>
+                <div class="meal-box">
+                  <h3>Dessert</h3>
+                  <div class="drop-recipe"></div>
+                </div>
+              </div>
+            </div>
+            <!-- soir -->
+            <div id="time-day" class="time-day">
+              <h4 class="title-meal">Soir</h4>
+              <div class="meal-box">
+                <h3>Entrée</h3>
+                <div class="drop-recipe"></div>
+              </div>
+              <div class="meal-box">
+                <h3>Plat</h3>
+                <div class="drop-recipe"></div>
+              </div>
+              <div class="meal-box">
+                <h3>Dessert</h3>
+                <div class="drop-recipe"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+  });
+};
+
+showPlanning();
+
+// gestion de la liste des recettes
+let selectRecipe;
 const recipeTag = () => {
-  const recipeBox = document.getElementById("recipe-box");
+  const recipeBox = document.getElementById("list-recipe-box");
 
   recipesWeek.forEach((recipe) => {
     const recipeTag = document.createElement("li");
-    recipeTag.classList.add("draggable", "tag-recipe");
+    recipeTag.classList.add("tag-recipe");
+    recipeTag.setAttribute("draggable", "true");
     recipeTag.innerText = recipe.nom;
     recipeBox.appendChild(recipeTag);
   });
+
+  // clone l'element et ajoute un evenement drag
+  let listRecipe = document.getElementsByClassName("tag-recipe");
+  for (let recipe of listRecipe) {
+    recipe.addEventListener("dragstart", (e) => {
+      let recipeTag = e.target.innerText;
+      let newElementTag = document.createElement("div");
+      newElementTag.classList.add("tag-recipe");
+      newElementTag.setAttribute("draggable", "true");
+
+      const newElementP = document.createElement("p");
+      newElementP.innerText = recipeTag;
+      newElementTag.appendChild(newElementP);
+
+      const newElementImg = document.createElement("img");
+      newElementImg.setAttribute("src", "../assets/img/icon-remove.png");
+      newElementImg.classList.add("remove-tag");
+      newElementTag.appendChild(newElementImg);
+      newElementImg.addEventListener("dragstart", (e) => {
+        e.preventDefault();
+      });
+      newElementImg.addEventListener("click", () => {
+        newElementTag.remove();
+      });
+
+      selectRecipe = newElementTag;
+    });
+  }
 };
 
 recipeTag();
 
-$(function () {
-  $(".sortable").sortable({
-    stop: function (event, ui) {
-      ui.item.removeAttr("style");
-    },
-  });
+// drag et drop
+const dropBox = () => {
+  let dropBox = document.getElementsByClassName("drop-recipe");
 
-  $(".draggable").draggable({
-    helper: "clone",
-    revert: "invalid",
-  });
+  for (let box of dropBox) {
+    box.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    box.addEventListener("drop", (e) => {
+      if (selectRecipe) {
+        box.innerHTML = "";
+        box.appendChild(selectRecipe);
+        selectRecipe.addEventListener("dragstart", (e) => {
+          selectRecipe = e.target;
+          console.log("Re-dragging:", selectRecipe);
+        });
+        selectRecipe = null;
+      }
+    });
+  }
+};
 
-  $(".drop-recipe").droppable({
-    accept: ".draggable",
-    drop: function (e, ui) {
-      //   console.log(ui.draggable);
-      const recipeName = ui.draggable.text();
-      $(this).append($("<li>").text(recipeName).addClass("tag-recipe"));
-    },
-  });
-
-  //   $(".choose-recipe")
-});
+dropBox();

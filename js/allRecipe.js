cb7502a5297ce6542bb00f9a078c78f5f67d3dc1
Bossola-Yannick <bossolay@hotmail.fr>
2025-03-31 11:16:50 +0200

    feat: added pagination for recipes sorted by category

--- js/allRecipe.js
+++ js/allRecipe.js
@@ -13,12 +13,61 @@ for (recipe of allRecipe) {
     dessert.push(recipe);
   }
 }
-for (const starterRecipe of starter) {
-  getRecipe(starterRecipe);
+
+// mise en tableau des recettes trié par catégorie
+const getAllRecipeSort = [];
+getAllRecipeSort.push(starter, dish, dessert);
+let allRecipeSort = getAllRecipeSort.flat();
+
+//----------------  PAGINATION  ------------------------
+// nombre recette par page
+const perPages = 9;
+// nombre de recette
+const nbRecipes = allRecipe.length;
+// calcul du nombre de page
+const nbPages = Math.ceil(nbRecipes / perPages);
+// récupération de l'url'
+const url = new URLSearchParams(location.search);
+// sur quelle page on se trouve
+let currentPage = parseInt(url.get("page")) || 1;
+console.log(currentPage);
+
+// mise en place de la vue de la pagination
+let previousPage = $("<a></a>")
+  .text("Précédent")
+  .attr({ class: "paging", href: `?page=${currentPage - 1}` });
+$("#paging").append(previousPage);
+for (let i = 1; i <= nbPages; i++) {
+  let linkPage = $("<a></a>")
+    .text(i)
+    .attr({ class: "paging", href: `?page=${i}` });
+  $("#paging").append(linkPage);
 }
-for (const dishRecipe of dish) {
-  getRecipe(dishRecipe);
+const nextPage = $("<a></a>")
+  .text("Suivant")
+  .attr({ class: "paging", href: `?page=${currentPage + 1}` });
+
+// conditionnement du précédent et suivant
+if (currentPage === 1) {
+  previousPage.text("").attr({ class: "disableLink" });
 }
-for (const dessertRecipe of dessert) {
-  getRecipe(dessertRecipe);
+if (currentPage === nbPages) {
+  nextPage.text("").attr({ class: "disableLink" });
 }
+// mise sur le DOM des élements
+$("#paging").append(nextPage);
+
+// fonction pour lancé la mise en card des recettes en fonction de la pages sélectionnée
+const displayRecipes = (listRecipesPerPage) => {
+  for (const recipe of listRecipesPerPage) {
+    getRecipe(recipe);
+  }
+};
+
+// données a afficher selon la page
+const recipesForThePage = allRecipeSort.slice(
+  perPages * (currentPage - 1),
+  perPages * currentPage
+);
+console.log(recipesForThePage);
+displayRecipes(recipesForThePage);

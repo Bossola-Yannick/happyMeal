f42e28bbfbc4e2cdbbeb9d35dd2cb7de329dfba3
James Sanchez <james.sanchez@laplateforme.io>
2025-04-02 14:50:06 +0200

    add: button on each favorite card to allow user to directly add recipe to planning list of recipe

--- js/favoris.js
+++ js/favoris.js
@@ -151,7 +151,7 @@ const createModal = (index) => {
 
   // event fermeture modale
   const modalClose = document.getElementById("modal-close");
-  modalClose.addEventListener("click", (e) => {
+  modalClose.addEventListener("click", () => {
     closeModal();
   });
 };
@@ -166,11 +166,16 @@ const showListFav = (categorie) => {
     listFavBox.innerHTML = "";
 
     favoritesRecipes.forEach((recipe) => {
+      const exists = recipesWeek.some(
+        (recipWeek) =>
+          recipWeek.nom === recipe.nom &&
+          recipWeek.categorie === recipe.categorie
+      );
+
       if (categorie === "Liste entière" || recipe.categorie === categorie) {
         const indexRecipe = favoritesRecipes.indexOf(recipe);
 
         // boite recette
-        // sous boite de chaque recette
         const card = document.createElement("div");
         card.id = "card-box";
         card.classList.add("card-box");
@@ -184,6 +189,10 @@ const showListFav = (categorie) => {
         } else if (recipe.categorie === "Dessert") {
           card.classList.add("dessert");
         }
+
+        // sous boite
+        const subCard = document.createElement("div");
+        subCard.classList.add("sub-card");
 
         // ajoute le bouton retirer favoris
         const cardButton = document.createElement("div");
@@ -215,10 +224,10 @@ const showListFav = (categorie) => {
         });
 
         cardButton.appendChild(buttonFav);
-        card.appendChild(cardButton);
+        // card.appendChild(cardButton);
 
         // event pour afficher la modale de la recette
-        card.addEventListener("click", () => {
+        subCard.addEventListener("click", () => {
           openModal();
           createModal(indexRecipe);
         });
@@ -227,7 +236,7 @@ const showListFav = (categorie) => {
         const imgCard = document.createElement("img");
         imgCard.setAttribute("src", `../assets/img/${recipe.image}`);
         imgCard.classList.add("image-recipe");
-        card.appendChild(imgCard);
+        // card.appendChild(imgCard);
 
         // ajoute le titre et la catégorie
         const cardTitle = document.createElement("div");
@@ -237,7 +246,7 @@ const showListFav = (categorie) => {
           <h3>${recipe.nom}</h3>
           <p>${recipe.categorie}</p>
         `;
-        card.appendChild(cardTitle);
+        // card.appendChild(cardTitle);
 
         // boite pour les ingrédients
         const cardDesc = document.createElement("div");
@@ -248,15 +257,79 @@ const showListFav = (categorie) => {
           pIngredient.textContent = ingredient.nom;
           cardDesc.appendChild(pIngredient);
         });
-        card.appendChild(cardDesc);
+
+        const cardDescTwo = document.createElement("div");
+        cardDescTwo.classList.add("card-desc-two");
+        cardDescTwo.innerHTML = `
+        <img src="../assets/img/icon-cook.png" />
+        <p>Préparation: ${recipe.temps_preparation}</p>`;
+
+        // card.appendChild(cardDesc);
+
+        // ajoute les elements img / titre / desc a la sub card
+        subCard.appendChild(cardButton);
+        subCard.appendChild(imgCard);
+        subCard.appendChild(cardTitle);
+        subCard.appendChild(cardDescTwo);
+        subCard.appendChild(cardDesc);
+
+        card.appendChild(subCard);
 
         // Ajoute le footer
         const cardFooter = document.createElement("div");
         cardFooter.id = "card-footer";
-        cardFooter.innerHTML = `
-        <img src="../assets/img/icon-cook.png" />
-        <p>Préparation: ${recipe.temps_preparation}</p>`;
+        cardFooter.innerHTML = ` 
+        ${
+          exists
+            ? `
+          <h4>Retirer des recettes de la semaine?</h4>
+          <button id="remove-recipe-day" type="submit" class="button-action" action="remove" value="${indexRecipe}">
+              <img src="../assets/img/icon-remove.png" />
+          </button>`
+            : `
+          <h4>Ajouter aux recettes de la semaine?</h4>
+          <button id="add-recipe-day" type="submit" class="button-action" action="add" value="${indexRecipe}">
+              <img src="../assets/img/icon-add.png" />
+          </button>`
+        }
+        `;
+
         card.appendChild(cardFooter);
+
+        cardFooter.addEventListener("click", (e) => {
+          e.stopPropagation();
+        });
+
+        // évènement boutons
+        const buttonAction = cardFooter.querySelector(".button-action");
+        buttonAction.addEventListener("click", (e) => {
+          // e.stopPropagation();
+          const button = e.currentTarget;
+          const action = button.getAttribute("action");
+          const recipeIndex = button.getAttribute("value");
+          const titleButton = cardFooter.querySelector("h4");
+          console.log(titleButton);
+
+          if (action === "add") {
+            // ajoute la recette aux recettes de la semaine
+            recipesWeek.push(favoritesRecipes[recipeIndex]);
+            localStorage.setItem("recipes-week", JSON.stringify(recipesWeek));
+
+            // change le bouton en bouton supprimer
+            button.setAttribute("action", "remove");
+            button.innerHTML = `<img src="../assets/img/icon-remove.png" />`;
+            titleButton.textContent = "Retirer des recettes de la semaine?";
+          } else if (action === "remove") {
+            // supprime la recette aux recettes de la semaine
+            recipesWeek.splice(recipeIndex, 1);
+            localStorage.setItem("recipes-week", JSON.stringify(recipesWeek));
+
+            // change le bouton en bouton ajouter
+            button.setAttribute("action", "add");
+            button.innerHTML = `<img src="../assets/img/icon-add.png" />`;
+            titleButton.textContent = "Ajouter aux recettes de la semaine?";
+          }
+        });
 
         // Ajoute la carte au conteneur principal
         // card.appendChild(card);
